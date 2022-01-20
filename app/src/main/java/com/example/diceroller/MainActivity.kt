e83434d81b57b39b47c08f366dc5e56421e30195
Ariel Alves <arielalves9@gmail.com>
2022-01-19 21:57:23 -0300

    add dice 2

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -4,15 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // insre a imagem no layout
+
+        // insre a imagem no layout quando o botão for clicado
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener { rollDice() }
 
@@ -23,9 +22,13 @@ class MainActivity : AppCompatActivity() {
 
     // Função que cria um dado de x lados
     private fun rollDice() {
-        val dice = Dice(6)
-        val diceRoll = dice.roll()
+        val dice1 = Dice(6)
+        val dice2 = Dice(6)
+        val diceRoll = dice1.roll()
+        val diceRoll2 = dice2.roll()
         val diceImage: ImageView = findViewById(R.id.imageView)
+        val diceImage2: ImageView = findViewById(R.id.Dice2)
+
         val drawableResource = when (diceRoll) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
@@ -35,9 +38,21 @@ class MainActivity : AppCompatActivity() {
             else -> R.drawable.dice_6
 
        }
+        val drawableResource2 = when (diceRoll2) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+
+        }
 
         diceImage.setImageResource(drawableResource)
+        diceImage2.setImageResource(drawableResource2)
         diceImage.contentDescription = diceRoll.toString()
+        diceImage2.contentDescription = diceRoll2.toString()
+
 
     }
 }
