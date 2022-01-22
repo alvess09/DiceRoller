5626701d9129ae98fcc9800aaf2b442f877d3dff
Ariel Alves <arielalves9@gmail.com>
2022-01-21 22:42:45 -0300

    add UnitTest in method roll()

--- app/src/test/java/com/example/diceroller/ExampleUnitTest.kt
+++ app/src/test/java/com/example/diceroller/ExampleUnitTest.kt
@@ -3,15 +3,14 @@ package com.example.diceroller
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Assert.assertTrue as assertTrue1
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 class ExampleUnitTest {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
-    }
+    fun generatesNumber() {
+        val dice =  Dice(6)
+        val rollResult = dice.roll()
+        assertTrue1("The value of rollResult was not between 1 and 6",rollResult in 1..6)
+
+   }
 }
